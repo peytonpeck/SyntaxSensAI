6ad7b9a6e04a0d30666600da53cd791d9dc7711f
Peyton Peck <peckpeyton1@hotmail.com>
2025-04-03 21:34:15 -0500

    backend service scaffolding

---
+++ backend/backend-service/src/test/java/com/syntaxsenai/backendservice/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxsenai.backendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
